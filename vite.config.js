599a094cbb07fed13688ff94d82a95ab8d6e84bf
nikoo28 <nikoo28@gmail.com>
2025-12-29 14:19:00 +0530

    config: setup github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/smart-interview-grind/',
   server: {
     allowedHosts: ["quiet-stone-1cec.tunnl.gg"],
   },
